a010b1108e43694b97fe1b3bfe5835c1b63deda4
Hoai-Thu Vuong <thuvh87@gmail.com>
2019-12-25 16:34:57 +0700

    prepare environment

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT=$HOME/projects/prometheus-demo
+PROMETHEUS_ROOT=$ROOT/prometheus
+PROMETHEUS_HOME=$PROMETHEUS_ROOT/prometheus-2.15.0.linux-amd64
+
+ALERTMANAGER_ROOT=$ROOT/alertmanager 
+ALERTMANAGER_HOME=$ALERTMANAGER_ROOT/alertmanager-0.20.0.linux-amd64
+
+PUSHGATEWAY_ROOT=$ROOT/pushgateway
+PUSHGATEWAY_HOME=$PUSHGATEWAY_ROOT/pushgateway-1.0.1.linux-amd64
+
+PATH=$PROMETHEUS_HOME:$ALERTMANAGER_HOME:$PATH
